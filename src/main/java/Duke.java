666bf39b2e13877c66d83aeb8d67a8bd95815701
tehkokhoe <tkhoe210@hotmail.com>
2022-01-20 23:05:51 +0800

    Level-4 Added enum and additional functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,17 @@
 import java.util.*;
 
 public class Duke {
+    private enum Command {
+        BYE,
+        LIST,
+        MARK,
+        UNMARK,
+        TODO,
+        DEADLINE,
+        EVENT
+        ;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -17,40 +28,58 @@ public class Duke {
         ArrayList<Task> list = new ArrayList<Task>(100);
         while(StillIn) {
             String in = fr.nextLine();
-            String[] input = in.split(" ");
-            Task task = new Task(in);
-            boolean printed = false;
+            String[] input = in.split(" ", 2);
+            Command cmd = Command.valueOf(input[0].toUpperCase());
             System.out.println("    ____________________________________________________________");
-            if(input[0].equals("bye")) {
-                System.out.println(bye());
-                StillIn = false;
-                printed = true;
-            } else if(input[0].equals("list")) {
-                list(list);
-                printed = true;
-            } else if(input[0].equals("mark")) {
-                int num = Integer.parseInt(input[1]);
-                list.get(num - 1).setDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[X] " + list.get(num - 1).getTask());
-                printed = true;
-            } else if(input[0].equals("unmark")) {
-                int num = Integer.parseInt(input[1]);
-                list.get(num - 1).setUndone();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("[ ] " + list.get(num - 1).getTask());
-            }
+            switch(cmd) {
+                case BYE:
+                    System.out.println(bye());
+                    StillIn = false;
+                    break;
 
-            if(printed) {
-                System.out.println("    ____________________________________________________________");
-                continue;
-            }
+                case LIST:
+                    list(list);
+                    break;
 
-            if(list.contains(task)) {
-                System.out.println(task);
-            } else {
-                list.add(task);
-                System.out.println("added: " + in);
+                case MARK:
+                    int numMark = Integer.parseInt(input[1]);
+                    list.get(numMark - 1).setDone();
+                    System.out.println("    Nice! I've marked this task as done:");
+                    System.out.println(list.get(numMark - 1));
+                    break;
+
+                case UNMARK:
+                    int numUnmark = Integer.parseInt(input[1]);
+                    list.get(numUnmark - 1).setUndone();
+                    System.out.println("    OK, I've marked this task as not done yet:");
+                    System.out.println(list.get(numUnmark - 1));
+                    break;
+
+                case TODO:
+                    Task todo = new ToDo(input[1]);
+                    list.add(todo);
+                    System.out.println("    Got it. I've added this task:");
+                    System.out.println(todo);
+                    System.out.printf("    Now you have %d task(s) in the list\n", list.size());
+                    break;
+
+                case DEADLINE:
+                    String bydate = input[1].split("/by ")[1];
+                    Task deadline = new Deadline(input[1], bydate);
+                    list.add(deadline);
+                    System.out.println("    Got it. I've added this task:");
+                    System.out.println(deadline);
+                    System.out.printf("    Now you have %d task(s) in the list\n", list.size());
+                    break;
+
+                case EVENT:
+                    String atdate = input[1].split("/at ")[1];
+                    Task event = new Event(input[1], atdate);
+                    list.add(event);
+                    System.out.println("    Got it. I've added this task:");
+                    System.out.println(event);
+                    System.out.printf("    Now you have %d task(s) in the list\n", list.size());
+                    break;
             }
             System.out.println("    ____________________________________________________________");
         }
@@ -58,7 +87,7 @@ public class Duke {
 
     public static void list(ArrayList<Task> lst) {
         for(int i=0; i < lst.size(); i++) {
-            System.out.printf("%d. %s\n", i + 1, lst.get(i));
+            System.out.printf("    %d. %s\n", i + 1, lst.get(i));
         }
     }
 
