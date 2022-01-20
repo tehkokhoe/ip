98dd74fd9aa393ccea588a9f7e7db6fbee641f46
tehkokhoe <tkhoe210@hotmail.com>
2022-01-20 21:00:38 +0800

    added new Class Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,46 @@
+public class Task {
+    private String task;
+    private boolean done;
+
+    public Task(String task) {
+        this.task = task;
+        this.done = false;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setDone() {
+        this.done = true;
+    }
+
+    public void setUndone() {
+        this.done = false;
+    }
+
+    @Override
+    public int hashCode() {
+        return task.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if(other instanceof Task){
+            Task toCompare = (Task) other;
+            return this.task.equals(toCompare.getTask());
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String temp;
+        if(this.done) {
+            temp = "X";
+        } else {
+            temp = " ";
+        }
+        return "[" + temp + "] " + this.task;
+    }
+}
