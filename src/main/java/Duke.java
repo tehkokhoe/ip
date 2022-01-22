cd2cec9ff7b7c86fb813ee63719bd30c2486f627
tehkokhoe <tkhoe210@hotmail.com>
2022-01-22 09:53:57 +0800

    Update accessor of enum, add error handling, and change display.
    Change the enum accessor so that it is accessible.
    Add error handling to prevent program from terminating due to illegal user input.
    Change display to look neater.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Duke {
-    private enum Command {
+    enum Command {
         BYE,
         LIST,
         MARK,
@@ -31,55 +31,96 @@ public class Duke {
             String[] input = in.split(" ", 2);
             Command cmd = Command.valueOf(input[0].toUpperCase());
             System.out.println("    ____________________________________________________________");
-            switch(cmd) {
-                case BYE:
-                    System.out.println(bye());
-                    StillIn = false;
-                    break;
+            try {
+                switch (cmd) {
+                    case BYE:
+                        System.out.println(bye());
+                        StillIn = false;
+                        break;
 
-                case LIST:
-                    list(list);
-                    break;
+                    case LIST:
+                        list(list);
+                        break;
 
-                case MARK:
-                    int numMark = Integer.parseInt(input[1]);
-                    list.get(numMark - 1).setDone();
-                    System.out.println("    Nice! I've marked this task as done:");
-                    System.out.println(list.get(numMark - 1));
-                    break;
+                    case MARK:
+                        if(input.length < 2) {
+                            throw new DukeException("    ☹ OOPS!!! I don't know what to mark");
+                        }
 
-                case UNMARK:
-                    int numUnmark = Integer.parseInt(input[1]);
-                    list.get(numUnmark - 1).setUndone();
-                    System.out.println("    OK, I've marked this task as not done yet:");
-                    System.out.println(list.get(numUnmark - 1));
-                    break;
+                        int numMark = Integer.parseInt(input[1]);
 
-                case TODO:
-                    Task todo = new ToDo(input[1]);
-                    list.add(todo);
-                    System.out.println("    Got it. I've added this task:");
-                    System.out.println(todo);
-                    System.out.printf("    Now you have %d task(s) in the list\n", list.size());
-                    break;
+                        if(numMark > list.size()) {
+                            throw new DukeException("    ☹ OOPS!!! I don't see your task");
+                        }
 
-                case DEADLINE:
-                    String bydate = input[1].split("/by ")[1];
-                    Task deadline = new Deadline(input[1], bydate);
-                    list.add(deadline);
-                    System.out.println("    Got it. I've added this task:");
-                    System.out.println(deadline);
-                    System.out.printf("    Now you have %d task(s) in the list\n", list.size());
-                    break;
+                        list.get(numMark - 1).setDone();
+                        System.out.println("    Nice! I've marked this task as done:");
+                        System.out.println("     " + list.get(numMark - 1));
+                        break;
 
-                case EVENT:
-                    String atdate = input[1].split("/at ")[1];
-                    Task event = new Event(input[1], atdate);
-                    list.add(event);
-                    System.out.println("    Got it. I've added this task:");
-                    System.out.println(event);
-                    System.out.printf("    Now you have %d task(s) in the list\n", list.size());
-                    break;
+                    case UNMARK:
+                        if(input.length < 2) {
+                            throw new DukeException("    ☹ OOPS!!! I don't know what to unmark");
+                        }
+
+                        int numUnmark = Integer.parseInt(input[1]);
+
+                        if(numUnmark > list.size()) {
+                            throw new DukeException("    ☹ OOPS!!! I don't see your task");
+                        }
+
+                        list.get(numUnmark - 1).setUndone();
+                        System.out.println("    OK, I've marked this task as not done yet:");
+                        System.out.println("     " + list.get(numUnmark - 1));
+                        break;
+
+                    case TODO:
+                        if(input.length < 2) {
+                            throw new DukeException("    ☹ OOPS!!! The description of a todo cannot be empty");
+                        }
+                        Task todo = new ToDo(input[1]);
+                        list.add(todo);
+                        System.out.println("    Got it. I've added this task:");
+                        System.out.println("     " + todo);
+                        System.out.printf("    Now you have %d task(s) in the list\n", list.size());
+                        break;
+
+                    case DEADLINE:
+                        if(input.length < 2) {
+                            throw new DukeException("    ☹ OOPS!!! The description of a deadline cannot be empty");
+                        }
+                        String[] splitbydate = input[1].split(" /by ");
+                        if(splitbydate.length < 2) {
+                            throw new DukeException("    ☹ OOPS!!! I don't know when your task is due");
+                        }
+                        String bydate = splitbydate[1];
+                        Task deadline = new Deadline(splitbydate[0], bydate);
+                        list.add(deadline);
+                        System.out.println("    Got it. I've added this task:");
+                        System.out.println("     " + deadline);
+                        System.out.printf("    Now you have %d task(s) in the list\n", list.size());
+                        break;
+
+                    case EVENT:
+                        if(input.length < 2) {
+                            throw new DukeException("    ☹ OOPS!!! The description of an event cannot be empty");
+                        }
+                        String[] splitatdate = input[1].split(" /at ");
+                        if(splitatdate.length < 2) {
+                            throw new DukeException("    ☹ OOPS!!! I don't know when your event happens");
+                        }
+                        String atdate = splitatdate[1];
+                        Task event = new Event(splitatdate[0], atdate);
+                        list.add(event);
+                        System.out.println("    Got it. I've added this task:");
+                        System.out.println("     " + event);
+                        System.out.printf("    Now you have %d task(s) in the list\n", list.size());
+                        break;
+                }
+            } catch(DukeException err) {
+                System.out.println(err.getMessage());
+            } catch(NumberFormatException err) {
+                System.out.println("    ☹ OOPS!!! Task number given is not suitable");
             }
             System.out.println("    ____________________________________________________________");
         }
@@ -87,7 +128,13 @@ public class Duke {
 
     public static void list(ArrayList<Task> lst) {
         for(int i=0; i < lst.size(); i++) {
-            System.out.printf("    %d. %s\n", i + 1, lst.get(i));
+            if(i == 0) {
+                System.out.println("    Here are the tasks in your list:");
+            }
+            System.out.printf("     %d. %s\n", i + 1, lst.get(i));
+        }
+        if(lst.size() == 0) {
+            System.out.println("    There's nothing in your list");
         }
     }
 
