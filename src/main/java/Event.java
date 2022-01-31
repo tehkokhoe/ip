613ba0f58e6373b5c9deb68eaea21c9f1d9fe784
tehkokhoe <tkhoe210@hotmail.com>
2022-01-31 21:54:44 +0800

    Add different display method for tasks for record purposes
    Add toRecord method for save method to write in a different display from the output on console

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,4 +10,9 @@ public class Event extends Task {
     public String toString() {
         return "[E]" + super.toString() + " (at: " + at + ")";
     }
+
+    @Override
+    public String toRecord() {
+        return "E | " + super.toRecord() + " | " + at;
+    }
 }
